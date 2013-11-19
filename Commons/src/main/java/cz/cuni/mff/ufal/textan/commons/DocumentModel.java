d99dfd2391dfad068d7017226db581ec06661250
Blem <p.fanta@seznam.cz>
2013-11-19 22:11:37 +0100

    Basic project layout

---
+++ Commons/src/main/java/cz/cuni/mff/ufal/textan/commons/DocumentModel.java
@@ -0,0 +1,9 @@
+package cz.cuni.mff.ufal.textan.commons;
+
+/**
+ * User: Petr Fanta
+ * Date: 19.11.13
+ * Time: 22:09
+ */
+public class DocumentModel {
+}
---
+++ Server/src/main/java/cz/cuni/mff/ufal/textan/server/AppEntry.java
@@ -0,0 +1,14 @@
+package cz.cuni.mff.ufal.textan.server;
+
+/**
+ * User: Petr Fanta
+ * Date: 19.11.13
+ * Time: 22:01
+ */
+
+public class AppEntry {
+
+    public static void main(String[] args) {
+
+    }
+}
