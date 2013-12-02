dc6e78f3128ff16e75566e5c90328beb3f8587c8
Václav Pernička <vpernicka@gmail.com>
2013-12-02 18:59:07 +0100

    Skripty na vytvoreni schematu
    Alias AliasOccurrence Document Object ObjectType

---
+++ database/Schemas/dbo/Tables/Alias.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Alias]
+(
+	id_alias int PRIMARY KEY, 
+	id_object int NOT NULL FOREIGN KEY
+		REFERENCES Object(id_object),
+	alias nvarchar NOT NULL
+)
---
+++ database/Schemas/dbo/Tables/Object.table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Object]
+(
+	id_object int PRIMARY KEY, 
+	id_object_type int NOT NULL FOREIGN KEY
+		REFERENCES ObjectType (id_object_type),
+	data nvarchar
+)
