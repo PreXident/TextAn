2ed589b419376df31276300bd12960c771e8c60f
Václav Pernička <vpernicka@gmail.com>
2013-12-05 12:27:05 +0100

    Update AliasOccurrence.table.sql
    As in Gdrive db_final3.png
    not tested

--- database/Schemas/dbo/Tables/AliasOccurrence.table.sql
+++ database/Schemas/dbo/Tables/AliasOccurrence.table.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[AliasOccurrence]
 (
+
+	id_alias_occurence int PRIMARY KEY,
 	id_alias int FOREIGN KEY
 		REFERENCES Alias(id_alias), 
 	id_document int FOREIGN KEY
 		REFERENCES Document(id_document), 
-	position int NOT NULL,
-	CONSTRAINT PK_ALIAS_OCCURRENCE
-		PRIMARY KEY (id_alias, id_document)
+	position int NOT NULL
 )
