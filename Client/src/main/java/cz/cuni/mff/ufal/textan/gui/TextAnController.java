3c857fa511a231b607d0137ae092e21d8fe5e9f0
PreXident <hujena@seznam.cz>
2014-01-12 19:13:30 +0100

    Added first prototype of graph visualization

--- Client/src/main/java/cz/cuni/mff/ufal/textan/gui/TextAnController.java
+++ Client/src/main/java/cz/cuni/mff/ufal/textan/gui/TextAnController.java
@@ -5,6 +5,22 @@ import cz.cuni.mff.ufal.textan.core.processreport.ProcessReportPipeline;
 import cz.cuni.mff.ufal.textan.gui.reportwizard.ReportWizardStage;
 import cz.cuni.mff.ufal.textan.gui.reportwizard.ReportWizardWindow;
 import cz.cuni.mff.ufal.textan.gui.reportwizard.StateChangedListener;
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.SparseMultigraph;
+import edu.uci.ics.jung.graph.util.EdgeType;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Paint;
+import java.awt.Stroke;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,15 +30,20 @@ import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
 import jfxtras.labs.scene.control.window.Window;
+import org.apache.commons.collections15.Transformer;
 
 /**
  * Controller for the TextAn application.
@@ -64,6 +85,74 @@ public class TextAnController implements Initializable {
     @FXML
     private void close() {
         Platform.exit();
+    }
+
+    @FXML
+    private void graph() {
+        final Stage s = new Stage();
+        s.setWidth(350);
+        s.setHeight(350);
+        final SwingNode sn = new SwingNode();
+        s.setScene(new Scene(new Pane(sn)));
+        //
+        // Graph<V, E> where V is the type of the vertices
+        // and E is the type of the edges
+        Graph<Integer, String> g = new SparseMultigraph<Integer, String>();
+        // Add some vertices. From above we defined these to be type Integer.
+        g.addVertex((Integer)1);
+        g.addVertex((Integer)2);
+        g.addVertex((Integer)3);
+        // Add some edges. From above we defined these to be of type String
+        // Note that the default is for undirected edges.
+        g.addEdge("Edge-A", 1, 2); // Note that Java 1.5 auto-boxes primitives
+        g.addEdge("Edge-B", 2, 3);
+        // Let's see what we have. Note the nice output from the
+        // SparseMultigraph<V,E> toString() method
+        System.out.println("The graph g = " + g.toString());
+        //
+        // The Layout<V, E> is parameterized by the vertex and edge types
+        Layout<Integer, String> layout = new CircleLayout(g);
+        layout.setSize(new Dimension(300,300)); // sets the initial size of the space
+        // The BasicVisualizationServer<V,E> is parameterized by the edge types
+        //BasicVisualizationServer<Integer,String> vv = new BasicVisualizationServer<>(layout);
+        VisualizationViewer<Integer,String> vv = new VisualizationViewer<Integer,String>(layout);
+
+        vv.setPreferredSize(new Dimension(350,350)); //Sets the viewing area size
+        //
+        // Setup up a new vertex to paint transformer...
+        Transformer<Integer,Paint> vertexPaint = new Transformer<Integer,Paint>() {
+            @Override
+            public Paint transform(Integer i) {
+                return Color.GREEN;
+                }
+        };
+        // Set up a new stroke Transformer for the edges
+        float dash[] = {10.0f};
+        final Stroke edgeStroke = new BasicStroke(1.0f, BasicStroke.CAP_BUTT,
+                BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
+        Transformer<String, Stroke> edgeStrokeTransformer = new Transformer<String, Stroke>() {
+            @Override
+            public Stroke transform(String s) {
+                return edgeStroke;
+            }
+            };
+        vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+        vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
+        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+        vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
+        vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);
+        //
+        // Create a graph mouse and add it to the visualization component
+        DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
+        gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
+        vv.setGraphMouse(gm);
+        //
+        try {
+        SwingUtilities.invokeAndWait(() -> {
+            sn.setContent(vv);
+        });
+        } catch (Exception e) { }
+        s.show();
     }
 
     @FXML
