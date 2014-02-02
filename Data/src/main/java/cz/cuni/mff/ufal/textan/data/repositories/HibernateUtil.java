7b6713dc2994b3adfd32c6db19b0920ed2316d66
Václav Pernička <vpernicka@gmail.com>
2014-02-02 16:56:55 +0100

    Updated deprecated things

--- Data/src/main/java/cz/cuni/mff/ufal/textan/data/repositories/HibernateUtil.java
+++ Data/src/main/java/cz/cuni/mff/ufal/textan/data/repositories/HibernateUtil.java
@@ -6,8 +6,13 @@
 
 package cz.cuni.mff.ufal.textan.data.repositories;
 
-import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+
 
 /**
  * Hibernate Utility class with a convenient method to get Session Factory
@@ -18,13 +23,19 @@ import org.hibernate.SessionFactory;
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
+    private static final ServiceRegistry serviceRegistry;
     
     static {
         try {
             // Create the SessionFactory from standard (hibernate.cfg.xml) 
             // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
+            //sessionFactory = new Configuration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();        
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            
+        } catch (HibernateException ex) {
             // Log the exception. 
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
