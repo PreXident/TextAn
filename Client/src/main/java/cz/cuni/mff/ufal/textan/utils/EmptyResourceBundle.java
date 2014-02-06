3f784c0559658357a30dc97d2fbf93a481900c1e
PreXident <hujena@seznam.cz>
2014-02-06 08:53:43 +0100

    Added forgotten files to previous localization commit

---
+++ Client/src/main/java/cz/cuni/mff/ufal/textan/utils/EmptyResourceBundle.java
@@ -0,0 +1,38 @@
+package cz.cuni.mff.ufal.textan.utils;
+
+import java.util.Enumeration;
+import java.util.NoSuchElementException;
+import java.util.ResourceBundle;
+
+/**
+ * Empty {@link ResourceBundle}. Always returns key back.
+ */
+public class EmptyResourceBundle extends ResourceBundle {
+
+    @Override
+    protected Object handleGetObject(final String key) {
+        return key;
+    }
+
+    @Override
+    public Enumeration<String> getKeys() {
+        return new EmptyEnumeration<>();
+    }
+
+    /**
+     * Empty {@link Enumeration}.
+     * @param <T> iterating over T
+     */
+    static private class EmptyEnumeration<T> implements Enumeration<T> {
+
+        @Override
+        public boolean hasMoreElements() {
+            return false;
+        }
+
+        @Override
+        public T nextElement() {
+            throw new NoSuchElementException();
+        }
+    }
+}
