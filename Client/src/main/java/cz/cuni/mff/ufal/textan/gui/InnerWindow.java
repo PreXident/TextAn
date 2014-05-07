957f6f403674da071c9d4fa5fe8ad3eef968c88a
PreXident <hujena@seznam.cz>
2014-05-07 11:42:35 +0200

    Maximizing/minimizing inner windows now resets mouse cursor. May help with mouse cursor issues (see #17)

--- Client/src/main/java/cz/cuni/mff/ufal/textan/gui/InnerWindow.java
+++ Client/src/main/java/cz/cuni/mff/ufal/textan/gui/InnerWindow.java
@@ -6,6 +6,7 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Bounds;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
@@ -151,6 +152,7 @@ public class InnerWindow extends Window {
      * (Un)bind size and position according to {@link #maximilized}.
      */
     protected void adjustMaximized() {
+        this.setCursor(Cursor.DEFAULT);
         if (maximized.get()) {
             unmaximizedHeight = getPrefHeight();
             unmaximizedWidth = getPrefWidth();
