2cdd9e5a698ba5ebe519196bac5a8d1d788c57f6
Blem <p.fanta@seznam.cz>
2014-05-28 23:33:44 +0200

    Fixed problem with JAXB, changed version of CXF to 3.0.0, client has no longer any dependency on CXF(only jaxb xjc adapter for datetime type)

--- Client/src/main/java/cz/cuni/mff/ufal/textan/core/Client.java
+++ Client/src/main/java/cz/cuni/mff/ufal/textan/core/Client.java
@@ -47,10 +47,6 @@ public class Client {
 
     private static final QName USERNAME_TOKEN_HEADER = new QName("http://models.commons.textan.ufal.mff.cuni.cz", "usernameToken");
 
-    {
-        System.setProperty( "com.sun.xml.bind.v2.bytecode.ClassTailor.noOptimize", "true");
-    }
-
     /**
      * Settings of the application. Handle with care, they're shared.
      */
