741f36eb804f824f0a7e8b62466edc37fb455efb
PreXident <hujena@seznam.cz>
2014-06-12 23:13:06 +0200

    Added spinner to graph view to change distance from center node

--- Client/src/main/java/cz/cuni/mff/ufal/textan/gui/graph/GraphViewController.java
+++ Client/src/main/java/cz/cuni/mff/ufal/textan/gui/graph/GraphViewController.java
@@ -3,16 +3,21 @@ package cz.cuni.mff.ufal.textan.gui.graph;
 import cz.cuni.mff.ufal.textan.core.Graph;
 import cz.cuni.mff.ufal.textan.core.graph.Grapher;
 import cz.cuni.mff.ufal.textan.gui.Utils;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Semaphore;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import jfxtras.labs.scene.control.BigDecimalField;
 
 /**
  * Controls GraphView.
@@ -34,11 +39,35 @@ public class GraphViewController extends GraphController {
     @FXML
     private ToggleButton pickButton;
 
+    @FXML
+    private BigDecimalField distanceField;
+
+    @FXML
+    private ToolBar toolbar;
+
+    @FXML
+    private HBox leftToolbar;
+
+    @FXML
+    private HBox rightToolbar;
+
     /** Localization container. */
     ResourceBundle resourceBundle;
 
     /** Graph container. */
     GraphView graphView;
+
+    final Semaphore lock = new Semaphore(1);
+
+    @FXML
+    private void newDistance() {
+        if (lock.tryAcquire()) {
+            final Node node = getMainNode();
+            node.setCursor(Cursor.WAIT);
+            grapher.setDistance(distanceField.getNumber().intValue());
+            new Thread(new GraphGetter(), "GraphGetter").start();
+        }
+    }
 
     @FXML
     private void pick() {
@@ -64,6 +93,8 @@ public class GraphViewController extends GraphController {
         this.resourceBundle = rb;
         stackPane.prefWidthProperty().bind(scrollPane.widthProperty());
         stackPane.prefHeightProperty().bind(scrollPane.heightProperty());
+        leftToolbar.prefWidthProperty().bind(toolbar.widthProperty().add(-25).divide(2));
+        rightToolbar.prefWidthProperty().bind(toolbar.widthProperty().add(-25).divide(2));
     }
 
     /**
@@ -73,32 +104,47 @@ public class GraphViewController extends GraphController {
     @Override
     public void setGrapher(final Grapher grapher) {
         super.setGrapher(grapher);
+        distanceField.setNumber(new BigDecimal(grapher.getDistance()));
         final Node node = getMainNode();
         node.setCursor(Cursor.WAIT);
-        final Task<Graph> task = new Task<Graph>() {
-            @Override
-            protected Graph call() throws Exception {
-                return grapher.getGraph();
-            }
-        };
-        task.setOnSucceeded(e -> {
-            final Graph g = task.getValue();
-            graphView = new GraphView(settings,
-                    g.getNodes(), g.getEdges(), grapher.getRootId());
-            stackPane.getChildren().add(graphView);
-            graphView.requestFocus();
-            node.setCursor(Cursor.DEFAULT);
-            scrollPane.requestFocus();
-        });
-        task.setOnFailed(e -> {
-            node.setCursor(Cursor.DEFAULT);
-            callWithContentBackup(() -> {
-                createDialog()
-                        .owner(getDialogOwner(root))
-                        .title(Utils.localize(resourceBundle, "page.load.error"))
-                        .showException(task.getException());
+        new Thread(new GraphGetter(), "GraphGetter").start();
+    }
+
+    /**
+     * Task for getting graph from server.
+     */
+    protected class GraphGetter extends Task<Graph> {
+
+        /**
+         * Only constructor.
+         */
+        public GraphGetter() {
+            setOnSucceeded(e -> {
+                final Graph g = getValue();
+                graphView = new GraphView(settings,
+                        g.getNodes(), g.getEdges(), grapher.getRootId());
+                stackPane.getChildren().clear();
+                stackPane.getChildren().add(graphView);
+                graphView.requestFocus();
+                getMainNode().setCursor(Cursor.DEFAULT);
+                scrollPane.requestFocus();
+                lock.release();
             });
-        });
-        new Thread(task, "Grapher").start();
+            setOnFailed(e -> {
+                getMainNode().setCursor(Cursor.DEFAULT);
+                callWithContentBackup(() -> {
+                    createDialog()
+                            .owner(getDialogOwner(root))
+                            .title(Utils.localize(resourceBundle, "page.load.error"))
+                            .showException(getException());
+                });
+                lock.release();
+            });
+        }
+
+        @Override
+        protected Graph call() throws Exception {
+            return grapher.getGraph();
+        }
     }
 }
