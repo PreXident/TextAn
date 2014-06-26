806539b7eabd753eac6831e94fc5cce5626f9971
PreXident <hujena@seznam.cz>
2014-06-26 15:14:29 +0200

    Relation Objects for hyper-edges have now proper colors (as in pipeline)

--- Client/src/main/java/cz/cuni/mff/ufal/textan/gui/graph/RelationObject.java
+++ Client/src/main/java/cz/cuni/mff/ufal/textan/gui/graph/RelationObject.java
@@ -16,7 +16,7 @@ public class RelationObject extends cz.cuni.mff.ufal.textan.core.Object {
      * @param relation blueprint
      */
     public RelationObject(final Relation relation) {
-        super(-1, new ObjectType(-1, relation.getType().getName()), Arrays.asList(relation.getType().getName()));
+        super(-1, new ObjectType(relation.getType().getId(), relation.getType().getName()), Arrays.asList(relation.getType().getName()));
         this.relation = relation;
     }
 
