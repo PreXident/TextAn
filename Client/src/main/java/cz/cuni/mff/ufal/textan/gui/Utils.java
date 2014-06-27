31fe1ac9f0903a01778610a1a19c4c4d53b56aa6
PreXident <hujena@seznam.cz>
2014-06-27 09:18:28 +0200

    The size and position of the main window when maximalized is not stored, but the information about the maximalization itself is.

--- Client/src/main/java/cz/cuni/mff/ufal/textan/gui/Utils.java
+++ Client/src/main/java/cz/cuni/mff/ufal/textan/gui/Utils.java
@@ -72,23 +72,27 @@ public class Utils {
      * @param stage stage to prepate
      * @param id identification in settings
      * @param settings application settings
+     * @param addListeners flag indicating whether size/position listeners should be added
      */
-    static public void prepareStage(final Stage stage, final String id, final Properties settings) {
+    static public void prepareStage(final Stage stage, final String id,
+            final Properties settings, final boolean addListeners) {
         stage.setWidth(Double.parseDouble(settings.getProperty(id + ".width", "300")));
         stage.setHeight(Double.parseDouble(settings.getProperty(id + ".height", "200")));
         stage.setX(Double.parseDouble(settings.getProperty(id + ".x", "0")));
         stage.setY(Double.parseDouble(settings.getProperty(id + ".y", "0")));
         //add listeners
-        final ChangeListener<Number> sizeListener = (e, newVal, oldVal) -> {
-            settings.setProperty(id + ".width", Double.toString(stage.getWidth()));
-            settings.setProperty(id + ".height", Double.toString(stage.getHeight()));
-            settings.setProperty(id + ".x", Double.toString(stage.getX()));
-            settings.setProperty(id + ".y", Double.toString(stage.getY()));
-        };
-        stage.widthProperty().addListener(sizeListener);
-        stage.heightProperty().addListener(sizeListener);
-        stage.xProperty().addListener(sizeListener);
-        stage.yProperty().addListener(sizeListener);
+        if (addListeners) {
+            final ChangeListener<Number> sizeListener = (e, newVal, oldVal) -> {
+                settings.setProperty(id + ".width", Double.toString(stage.getWidth()));
+                settings.setProperty(id + ".height", Double.toString(stage.getHeight()));
+                settings.setProperty(id + ".x", Double.toString(stage.getX()));
+                settings.setProperty(id + ".y", Double.toString(stage.getY()));
+            };
+            stage.widthProperty().addListener(sizeListener);
+            stage.heightProperty().addListener(sizeListener);
+            stage.xProperty().addListener(sizeListener);
+            stage.yProperty().addListener(sizeListener);
+        }
     }
 
     /**
