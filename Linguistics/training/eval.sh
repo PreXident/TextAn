56029d8f10b76bd97b31611c10bfba2d0a551d7d
Blem <p.fanta@seznam.cz>
2014-07-07 20:10:59 +0200

    Removed unused scripts

--- Linguistics/training/eval.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-./run_ner -v czech-140205-cnec2.0.ner <cnec2.0-all/dtest.txt >czech-ner-140205-cnec2.0.dtest.entities
-./compare_ne_outputs_v3.pl cnec2.0-all/dtest.all.entities czech-ner-140205-cnec2.0.dtest.entities
