218eb83f9120b79709c9e3f8a463d94045e2616a
Václav Pernička <vpernicka@gmail.com>
2014-08-14 02:01:24 +0200

    Added edit date in audit table

--- Database/create.sql
+++ Database/create.sql
@@ -15,6 +15,7 @@ VALUES ( 0 );
 CREATE TABLE Audit (
 	id_audit int PRIMARY KEY AUTO_INCREMENT, 
   username NVARCHAR(255) NOT NULL,
+  edit_date datetime NOT NULL,
   edittype VARCHAR(255) NOT NULL,            -- INSERT | DELETE | UPDATE
   edit text CHARSET utf8 NOT NULL
 );
