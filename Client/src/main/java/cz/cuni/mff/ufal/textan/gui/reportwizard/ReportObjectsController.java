c6c3942001ee02497b0430513925c0fb2f498d14
PreXident <hujena@seznam.cz>
2014-10-26 11:17:25 +0100

    Replaced forgotten javafx.scene.text.Text by custom Text with hack

--- Client/src/main/java/cz/cuni/mff/ufal/textan/gui/reportwizard/ReportObjectsController.java
+++ Client/src/main/java/cz/cuni/mff/ufal/textan/gui/reportwizard/ReportObjectsController.java
@@ -47,7 +47,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.util.Callback;
 
 /**
